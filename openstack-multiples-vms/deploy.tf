009116dea9aa9de7c2e4df55006b736cc8fbc54e
Vanderson <vanderson@mercurio.localdomain>
2022-10-28 19:05:45 -0300

    terraform openstack

--- openstack-multiples-vms/deploy.tf
+++ openstack-multiples-vms/deploy.tf
@@ -11,10 +11,10 @@ terraform {
 provider "openstack" {
   #cloud = "opentack"
   auth_url      = "https://identity.spo1.edge.embratel.cloud:5000/v3"
-  user_name     = "vanderson.david"
-  token         = "gAAAAABjWtRTEDV1HzI71eE8EK5ptqwsp8PQohzNDXSavV115jvKrr1w9EnWuBJmsMOJ8h-_b_PCm2b9wGtm1Sy4s3bWum3jA4yKpMnhJ1H-WndR7NLnnosz-tbHsIuS3oyI7W0U2RDEt6Mw4DD2qpNScxWlaiHtUn3UzfR-QdBroRtM1Fj0LRbbuC6ByEBGBTUeUXtLCjUiVWT7Y1givyeqr-kKUm9hXcE78_T3aJKw-mppafKsPt8"
-  tenant_name   = "0000001-project"
-  domain_name   = "0000001-domain"
+  user_name     = "username"
+  token         = "token"
+  tenant_name   = "tenant"
+  domain_name   = "domain"
   region        =  "spo1"
 
 }
